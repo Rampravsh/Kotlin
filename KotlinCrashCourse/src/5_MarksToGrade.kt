06b7a8aba2b10b31f891a79ee3d81f9af38d994f
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-09-20 16:57:45 +0530

    add example of when in kotlin

--- KotlinCrashCourse/src/5_MarksToGrade.kt
+++ KotlinCrashCourse/src/5_MarksToGrade.kt
@@ -1,15 +1,25 @@
 fun main(){
     print("Enter marks: ")
     val marks = readln().toInt()
-    if (marks in 91..100){
-        println("Grade A")
-    }else if (marks in 81..90){
-        println("Grade B")
-    }else if(marks in 71..80){
-        println("Grade C")
-    }else{
-        println("F")
+//    if (marks in 91..100){
+//        println("Grade A")
+//    }else if (marks in 81..90){
+//        println("Grade B")
+//    }else if(marks in 71..80){
+//        println("Grade C")
+//    }else{
+//        println("F")
+//    }
+
+    val grade :Char = when(marks){
+        in 91..100->'A'
+        in 81..90 ->'B'
+        in 71..80 ->'C'
+        else ->{
+            'F'
+        }
     }
 
+    print("Grade is $grade")
 
 }
