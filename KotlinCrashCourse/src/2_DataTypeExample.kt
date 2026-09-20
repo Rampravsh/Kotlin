04831f067db3d2b53fc567cd6cc716ac4b20ce1c
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-09-20 16:40:49 +0530

    Data type example

--- KotlinCrashCourse/src/2_DataTypeExample.kt
+++ KotlinCrashCourse/src/2_DataTypeExample.kt
@@ -1,2 +1,24 @@
-class `2_DataTypeExample` {
+import java.net.InetAddress
+
+class User(
+    val name: String,
+    val phone: String,
+    val address: String
+){
+    fun printAll(){
+        println(name)
+        println(phone)
+        println(address)
+    }
+}
+
+
+fun main(){
+    val user1:User =User("ram", "478320478998","patna")
+
+    println(user1.address)
+    println(user1.name)
+    println(user1.phone)
+
+    user1.printAll()
 }
