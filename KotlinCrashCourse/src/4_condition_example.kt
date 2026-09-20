5a82e0afc4be0a530189a6342df2db9197f65e43
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-09-20 16:42:23 +0530

    example on condition if else

--- KotlinCrashCourse/src/4_condition_example.kt
+++ KotlinCrashCourse/src/4_condition_example.kt
@@ -1,2 +1,12 @@
-class `4_condition_example` {
+
+fun main(){
+    print("Enter number: ")
+    val number: Int = readln().toInt()
+    if (number>0){
+        println("$number Number is Positive")
+    }else if (number<0){
+        println("$number Number is Negative ")
+    }else{
+        println("number is Zero")
+    }
 }
