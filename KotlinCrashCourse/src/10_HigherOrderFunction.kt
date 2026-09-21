4adb1a89ed3d4350e0c9f88ba31b6a7143c7ed17
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-09-21 13:46:18 +0530

    example of higher order function

--- KotlinCrashCourse/src/10_HigherOrderFunction.kt
+++ KotlinCrashCourse/src/10_HigherOrderFunction.kt
@@ -1,2 +1,19 @@
-class `10_HigherOrderFunction` {
+fun sumTwoNumber(num1:Int, num2: Int, callback:()-> Unit){
+    println("Sum of two numbers is ${num1+num2}")
+    callback()
+}
+
+fun cube(num: Int, callback: (message: String) -> Unit){
+    println("Cube is ${num*num*num}")
+    callback("cube calculated")
+}
+
+fun main(){
+    val callBackFuncion = {
+        println("sum completed")
+    }
+
+    sumTwoNumber(23,423,callBackFuncion)
+
+    cube(3){ println(it)}
 }
