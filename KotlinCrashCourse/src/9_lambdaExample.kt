9cbd0b4f66791b6a7f636e427b0b16945a4c3623
Rampravesh kumar <rampraveshkr4545@gmail.com>
2026-09-21 13:45:45 +0530

    example of lambda function

--- KotlinCrashCourse/src/9_lambdaExample.kt
+++ KotlinCrashCourse/src/9_lambdaExample.kt
@@ -1,2 +1,13 @@
-class `9_lambdaExample` {
+fun main (){
+
+    val square = {x: Int ->x*x }
+    val addition ={x:Int, y:Int ->x+y}
+    val myVar = {
+        println("hi i am lambda")
+    }
+    myVar()
+
+    println(square(5))
+    println(addition(35, 34))
 }
+
